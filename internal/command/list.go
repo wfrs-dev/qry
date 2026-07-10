e7aa97817a86a627529e56afe5ee1c2283ac369a
Walter Fabian Rodriguez Salazar <wfrodriguez@gmail.com>
2026-07-10 16:02:59 -0500

    feat: Agregar argumento -name al comando list

--- internal/command/list.go
+++ internal/command/list.go
@@ -11,11 +11,13 @@ import (
 
 type ListCommand struct {
 	*flaggy.Subcommand
+	name bool
 }
 
 func (cmd *ListCommand) Setup() *flaggy.Subcommand {
 	cmd.Subcommand = flaggy.NewSubcommand(cmd.Name())
 	cmd.Subcommand.Description = "Lista las conexiones guardadas"
+	cmd.Subcommand.Bool(&cmd.name, "n", "name", "Muestra solo los nombres de las conexiones")
 
 	return cmd.Subcommand
 }
@@ -30,9 +32,16 @@ func (cmd *ListCommand) Execute(ctx *console.Context) error {
 		return errors.New("No hay conexiones guardadas")
 	}
 
-	fmt.Printf(" %-20s %-15s %s\n", "NOMBRE", "DRIVER", "DSN")
-	fmt.Println("----------------------------------------------------------")
+	if !cmd.name {
+		fmt.Printf(" %-20s %-15s %s\n", "NOMBRE", "DRIVER", "DSN")
+		fmt.Println("----------------------------------------------------------")
+	}
 	for name, conn := range conns {
+		if cmd.name {
+			fmt.Println(name)
+			continue
+		}
+
 		fmt.Printf(" %-20s %-15s %s\n", name, conn.Driver, conn.MaskedDSN())
 	}
 
